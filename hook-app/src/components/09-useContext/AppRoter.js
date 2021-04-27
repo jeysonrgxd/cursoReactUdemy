326aab3630eb039da15f4697bf2809ad24190a49
= <=>
2021-04-27 00:33:56 -0500

    router components

---
+++ hook-app/src/components/09-useContext/AppRoter.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom'
+
+import { AboutScreen } from './AboutScreen'
+import { HomeScreen } from './HomeScreen'
+import { LoginScreen } from './LoginScreen'
+
+// este componente se encargara de mostrar y trabajar las rutas
+
+export const AppRoter = () => {
+    return (
+        <Router>
+            <div>
+
+                {/* Ponemos un switch para especificar la ruta con el componente a cargar */}
+                <Switch>
+                    <Route exact path="/" component={HomeScreen}></Route>
+                    <Route exact path="/about" component={AboutScreen}></Route>
+                    <Route exact path="/login" component={LoginScreen}></Route>
+                </Switch>
+            </div>
+        </Router>
+    )
+}
--- hook-app/src/components/09-useContext/MainApp.js
+++ hook-app/src/components/09-useContext/MainApp.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { AppRoter } from './AppRoter'
 
 export const MainApp = () => {
    return (
       <div>
-         <h1>MainApp</h1>
+         {/* <h1>MainApp</h1> */}
+         <AppRoter></AppRoter>
       </div>
    )
 }

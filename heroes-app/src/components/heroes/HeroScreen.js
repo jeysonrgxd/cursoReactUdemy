24d4a20f2f95485013af893ba9c2766ed68d525f
= <=>
2021-05-06 00:24:59 -0500

    obteniendo params

--- heroes-app/src/components/heroes/HeroScreen.js
+++ heroes-app/src/components/heroes/HeroScreen.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useParams } from 'react-router'
+import { getHeroById } from '../../selectors/getHeroById'
 
-export const HeroScreen = () => {
+// destructuramos el argumento prop para obtener el heroe, se puede ver mejor en la consola 
+//del navegador mirando los componentes
+export const HeroScreen = ({match}) => {
+
+    //usamos el hook de react router para obtener parametros de url
+    const {heroId} = useParams()
+
+    // console.log(match.params)
+    console.log(heroId)
+    const heroe = getHeroById(heroId)
+    console.log(heroe)
+
     return (
         <div>
             <h1>HeroScreen</h1>
+
+            <pre>
+                {JSON.stringify(heroe,null,3)}
+            </pre>
         </div>
     )
 }

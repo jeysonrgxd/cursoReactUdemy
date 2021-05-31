69c7575312cf779a2789105a91bb32b1a2a9da39
jeyson <jeysonrgxd@gmail.com>
2021-05-30 22:48:49 -0500

    Notes Screem formulario para crear notas

--- 06-journal-app/src/components/journal/Sidebar.js
+++ 06-journal-app/src/components/journal/Sidebar.js
@@ -5,19 +5,19 @@ export const Sidebar = () => {
     return (
 
         <aside className="journal__sidebar">
-            <div class="journal__sidebar-navbar">
+            <div className="journal__sidebar-navbar">
                 <h3 className="mt-5">
                     <i className="far fa-moon"></i>
                     <span> Jeyson</span>
                 </h3>
 
-                <button class="btn">
+                <button className="btn">
                     Logout
                 </button>
             </div>
 
 
-            <div class="jounal__new-entry">
+            <div className="jounal__new-entry">
                 <i className="far fa-calendar-plus fa-5x"></i>
                 <p className="mt-5">
                     New entry
--- 06-journal-app/src/components/notes/NotesScreem.js
+++ 06-journal-app/src/components/notes/NotesScreem.js
@@ -5,6 +5,24 @@ export const NotesScreem = () => {
     return (
         <div className="notes__main-content">
             <NotesAppBar />
+            <div className="notes__content">
+                <input
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                />
+                <textarea
+                    placeholder="What happened today"
+                    className="notes__textarea"
+                ></textarea>
+
+                <div className="notes__image">
+                    <img
+                        src="https://lasimagenesdegoku.com/wp-content/uploads/2017/08/Goku_ssj_1_by_gokusuke.jpg"
+                        alt="image goku"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
